0084745bd5d59ac708e71e548ca952b1f61016c4
ambrosia-fish <josefameur@gmail.com>
2025-05-22 10:41:00 -0400

    fix: delete repo to restart development

--- src/lib/stores/auth.ts
+++
@@ -1,6 +0,0 @@
-// src/lib/stores/auth.ts
-import { writable } from 'svelte/store';
-
-// Simple auth stores
-export const isAuthenticated = writable(false);
-export const accessToken = writable('');
